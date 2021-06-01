6704b1faadc1701b196dbbda64e035d2d5355d82
kkcchh9999 <kkcchh9999@naver.com>
2021-06-02 00:47:59 +0900

    test

--- HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //메인 액티비티
     }
 }
