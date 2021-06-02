1e8133886b70769ffd06d48729fcba42edd5f080
kkcchh9999 <kkcchh9999@naver.com>
2021-06-02 09:13:24 +0900

    12 - 2

--- HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mWelcomeTextView: TextView //지연 초기화를 위한 선언
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //메인 액티비티
+
+        mWelcomeTextView = findViewById(R.id.tv_hello)  //지연 초기화 시점
     }
 }
