873e6108811022dbe12628993fc96571b1365eca
kkcchh9999 <kkcchh9999@naver.com>
2021-06-02 09:19:14 +0900

    12 - 3

--- HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -15,3 +15,29 @@ class MainActivity : AppCompatActivity() {
         mWelcomeTextView = findViewById(R.id.tv_hello)  //지연 초기화 시점
     }
 }
+
+/*
+제어문 옆에는 소괄호 사용시 공백을 둠
+while (true) {
+    toDo()
+}
+
+비어 있는 블록 구성시에도 중괄호는 내려서 씀
+if (true) {
+}
+
+예약된 키워드에 괄호가 사용되는 경우 공백 같이 사용
+for(i in 0..1) {
+} -> 틀림
+for (i in 0..1) {
+} -> 좋음
+
+중괄호 사용시, 이진 연산자 사용시 공백 추가
+if (true){
+} -> 틀림
+if (true) {
+} -> 좋음
+
+val two = 1+1 -> 틀림
+val two = 1 + 1 -> 좋음
+ */
