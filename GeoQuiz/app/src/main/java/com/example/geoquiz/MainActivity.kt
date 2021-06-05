5e31cb13e7fd05582ca1bbef712ca775a625723b
kkcchh9999 <kkcchh9999@naver.com>
2021-06-05 22:45:27 +0900

    Chapter 2 모델 - 뷰 - 컨트롤러

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -5,33 +5,84 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previousButton: ImageButton //챌린지 2
+    private lateinit var questionTextView: TextView
 
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)  //화면에 레이아웃을 inflate 하여 나타낸다.
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        previousButton = findViewById(R.id.previous_button) //챌린지 2
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
-            var myToast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
-            myToast.setGravity(Gravity.CENTER, 0, 0)
-            println(myToast.gravity)    //sdk30 이후로 더이상 toast 메세지의 gravity 편집 불가능
-            myToast.show()
+            checkAnswer(true)
         }
 
-        falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+        falseButton.setOnClickListener {
+            checkAnswer(false)
         }
+
+        //챌린지 1
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        previousButton.setOnClickListener {     //챌린지 2
+            if (currentIndex == 0) {
+                currentIndex = questionBank.size - 1
+                updateQuestion()
+            }
+            else {
+                currentIndex = (currentIndex - 1) % questionBank.size
+                updateQuestion()
+            }
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
