d94242b00b6b3b0ecde938c855f60d0bf775d0f4
kkcchh9999 <kkcchh9999@naver.com>
2021-07-06 17:36:46 +0900

    GeoQuiz 복습, 2장 내용 구현

--- GeoQuiz_Review/app/src/main/java/com/example/geoquiz_review/MainActivity.kt
+++ GeoQuiz_Review/app/src/main/java/com/example/geoquiz_review/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.geoquiz_review
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,6 +12,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvQuiz: TextView
     private lateinit var btnTrue: Button
     private lateinit var btnFalse: Button
+    private lateinit var btnNext: ImageButton
+    private lateinit var btnPrevious: ImageButton
+
+    private val quizList = listOf(
+        Quiz(R.string.quiz_australia, true),
+        Quiz(R.string.quiz_oceans, true),
+        Quiz(R.string.quiz_mideast, false),
+        Quiz(R.string.quiz_africa, false),
+        Quiz(R.string.quiz_america, true),
+        Quiz(R.string.quiz_asia, true)
+    )
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +32,9 @@ class MainActivity : AppCompatActivity() {
         //----- findViewById -----
         btnTrue = findViewById(R.id.btn_true)
         btnFalse = findViewById(R.id.btn_false)
-
+        tvQuiz = findViewById(R.id.tv_quiz)
+        btnNext = findViewById(R.id.btn_next)
+        btnPrevious = findViewById(R.id.btn_previous)
 
         //----- 이벤트 리스너 -----
         btnTrue.setOnClickListener {
@@ -38,5 +53,23 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
 
+        btnNext.setOnClickListener {
+            currentIndex = (currentIndex + 1) % quizList.size
+            updateUI()
+        }
+
+        btnPrevious.setOnClickListener {
+            currentIndex = if (currentIndex != 0) {
+                (currentIndex - 1) % quizList.size
+            } else {
+                5
+            }
+            updateUI()
+        }
+        updateUI()
+    }
+
+    private fun updateUI() {
+        tvQuiz.setText(quizList[currentIndex].textResID)
     }
 }
---
+++ GeoQuiz_Review/app/src/main/java/com/example/geoquiz_review/Quiz.kt
@@ -0,0 +1,5 @@
+package com.example.geoquiz_review
+
+import androidx.annotation.StringRes
+
+data class Quiz (@StringRes val textResID: Int, val answer: Boolean)
