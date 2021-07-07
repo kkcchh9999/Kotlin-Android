796fa28c023f931e63ccdfb3b35070bf150361f1
kkcchh9999 <kkcchh9999@naver.com>
2021-07-07 15:50:23 +0900

    GeoQuiz 복습, 3장 내용 구현

--- GeoQuiz_Review/app/src/main/java/com/example/geoquiz_review/MainActivity.kt
+++ GeoQuiz_Review/app/src/main/java/com/example/geoquiz_review/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.geoquiz_review
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
@@ -24,6 +26,8 @@ class MainActivity : AppCompatActivity() {
         Quiz(R.string.quiz_asia, true)
     )
     private var currentIndex = 0
+    private var userInputCount = 0.0    //----- chap3 챌린지 2 점수 표기하기 -----
+    private var userCorrectCount = 0    //----- chap3 챌린지 2 점수 표기하기 -----
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,19 +42,13 @@ class MainActivity : AppCompatActivity() {
 
         //----- 이벤트 리스너 -----
         btnTrue.setOnClickListener {
-            Toast.makeText(
-                this,
-                R.string.correct_message,
-                Toast.LENGTH_SHORT
-            ).show()
+            userInputCount ++   //----- chap3 챌린지 2 점수 표기하기 -----
+            checkAnswer(true)
         }
 
         btnFalse.setOnClickListener {
-            Toast.makeText(
-                this,
-                R.string.incorrect_message,
-                Toast.LENGTH_SHORT
-            ).show()
+            userInputCount ++   //----- chap3 챌린지 2 점수 표기하기 -----
+            checkAnswer(false)
         }
 
         btnNext.setOnClickListener {
@@ -69,7 +67,45 @@ class MainActivity : AppCompatActivity() {
         updateUI()
     }
 
+    //----- UI 업데이트 -----
     private fun updateUI() {
         tvQuiz.setText(quizList[currentIndex].textResID)
+        //----- chap3 챌린지1 맞춘 문제 true, false 버튼 안보이게하기 -----
+        if (quizList[currentIndex].correctCount) {
+            btnTrue.visibility = View.INVISIBLE
+            btnFalse.visibility = View.INVISIBLE
+        } else {
+            btnTrue.visibility = View.VISIBLE
+            btnFalse.visibility = View.VISIBLE
+        }
+    }
+
+    private fun checkAnswer (userAnswer: Boolean) {
+        if (quizList[currentIndex].answer == userAnswer) {
+            Toast.makeText(
+                this,
+                R.string.correct_message,
+                Toast.LENGTH_SHORT
+            ).show()
+            quizList[currentIndex].correctCount = true  //----- chap3 챌린지1 맞춘 문제 true, false 버튼 안보이게하기 -----
+            currentIndex = (currentIndex + 1) % quizList.size
+            userCorrectCount ++ //----- chap3 챌린지 2 점수 표기하기 -----
+            updateUI()
+        } else {
+            Toast.makeText(
+                this,
+                R.string.incorrect_message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        //----- chap3 챌린지 2 점수 표기하기 -----
+        if (userCorrectCount == quizList.size) {
+            Toast.makeText(
+                this,
+                "정답률: " + userCorrectCount / userInputCount * 100 + "%",
+                Toast.LENGTH_SHORT
+            ).show()
+            Log.d("이게무슨일이야", "정답카운터"+userCorrectCount+"인풋카운터"+userInputCount)
+        }
     }
 }
