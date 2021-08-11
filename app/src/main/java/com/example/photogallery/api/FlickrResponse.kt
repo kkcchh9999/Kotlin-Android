cdad39b8f200ea0794ccb3b7770b5d466bf29773
kkcchh9999 <kkcchh9999@naver.com>
2021-08-11 15:10:04 +0900

    폴더 수정

--- app/src/main/java/com/example/photogallery/api/FlickrResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.photogallery.api
-
-class FlickrResponse {
-    lateinit var photos: PhotoResponse
-}
--- app/src/main/java/com/example/photogallery/api/PhotoResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.photogallery.api
-
-import com.example.photogallery.GalleryItem
-import com.google.gson.annotations.SerializedName
-
-class PhotoResponse {
-    @SerializedName("photo")
-    lateinit var galleryItems: List<GalleryItem>
-}
