ae548cb04fa49fdd84cdb6e0f5e7f8f4753a6dad
kkcchh9999 <kkcchh9999@naver.com>
2021-08-16 23:28:52 +0900

    chap 24, PhotoGallery_review 생성

---
+++ PhotoGallery_review/app/src/main/java/com/example/photogallery_review/PhotoGalleryActivity.kt
@@ -0,0 +1,19 @@
+package com.example.photogallery_review
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoGalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_gallery)
+
+        val isFragmentContainerEmpty = savedInstanceState == null   //저장된 정보가 null 이면 fragment 생성, 현재 액티비티에 적용
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
+    }
+}
